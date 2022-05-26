7d412a6cf1a0955b81bef1f099dba41c25f5c811
regres <colewangyu@gmail.com>
2022-05-26 23:36:02 +0800

    log date.

--- elector/src/main/java/dataAccessor/FileAccessor.java
+++ elector/src/main/java/dataAccessor/FileAccessor.java
@@ -3,6 +3,7 @@ package dataAccessor;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.Date;
 import java.util.List;
 
 public class FileAccessor implements DataAccessor {
@@ -11,10 +12,12 @@ public class FileAccessor implements DataAccessor {
         try {
             FileOutputStream electeeFileOS = new FileOutputStream("electee.txt",true);
             OutputStreamWriter electeeOSW = new OutputStreamWriter(electeeFileOS, "UTF-8");
+            electeeOSW.write(String.valueOf(new Date()));
             for (String electee: electees) {
                 electeeOSW.write("\n");
                 electeeOSW.write(electee);
             }
+            electeeOSW.write("\n");
             electeeOSW.flush();
             electeeOSW.close();
         } catch (IOException e) {
