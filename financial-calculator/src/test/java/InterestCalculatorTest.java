1cf2df20f67b7ce085da7d59ef421f97a08f7e37
regres <colewangyu@gmail.com>
2022-08-24 16:18:17 +0800

    correct interest calculator

--- financial-calculator/src/test/java/InterestCalculatorTest.java
+++ financial-calculator/src/test/java/InterestCalculatorTest.java
@@ -4,13 +4,14 @@ class InterestCalculatorTest {
 
     @Test
     void calculateAutomobileLoanResult() {
-        InterestCalculator interestCalculator = new InterestCalculator(102000, 0, 0.027/12, 24, 3000);
+        InterestCalculator interestCalculator = new InterestCalculator(102000, 0, 0.04/12, 24, 3000);
         System.out.println("Income: " + interestCalculator.calculateMargin(interestCalculator.getLoanAmount() - interestCalculator.getServiceCharge(), 1, 0));
     }
 
     @Test
     void calculateTruckSpaceLoanResult() {
-        InterestCalculator interestCalculator = new InterestCalculator(100000, 0.036, 0.058/12, 60, 0);
+        InterestCalculator interestCalculator = new InterestCalculator(100000, 0.036, 0.04/12, 60, 0);
+        System.out.println("InterestIncome: " + interestCalculator.calculateInterestIncome(interestCalculator.getLoanAmount() - interestCalculator.getServiceCharge(), 1, 0));
         System.out.println("Income: " + interestCalculator.calculateMargin(interestCalculator.getLoanAmount() - interestCalculator.getServiceCharge(), 1, 0));
     }
 }
